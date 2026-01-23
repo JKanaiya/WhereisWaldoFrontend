2b5aee47e0a6a39b02d34bf5ad261c1748606153
JKanaiya <jonathankanaiya@gmail.com>
2026-01-23 13:53:22 +0300

    Feat: Added apiCalls.ts

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Node.js + Prisma development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_20
+        nodePackages.pnpm
+        typescript
+      ];
+
+      shellHook = ''
+          exec zsh
+        +  '';
+    };
+  };
+}
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,137 @@
-import { useId, useState } from 'react';
-import styles from './App.module.css'
-import './styles/reset.css'
-import pictures from '../assets/pictureData.json';
-import ApiCall from './apiCalls.js'
+import { useId, useState } from "react";
+import styles from "./App.module.css";
+import "./styles/reset.css";
+import pictures from "../assets/pictureData.json";
+import ApiCall from "./apiCalls.js";
+import React from "react";
 
-
-function App() {
+function App(): React.FC {
   const [box, setBox] = useState({ active: false, location: { x: 0, y: 0 } });
-  const [winState, setWinState] = useState(false)
+  const [winState, setWinState] = useState(false);
   // TODO: the below would be used in the case that i were to add a picture. It would be used to switch the current picture to the desired one
   // const [picture, setPicture] = useState();
   const randName = useId();
 
-  const toggleBoxActive = (ev: { clientX: number, clientY: number }): void | false => {
+  const toggleBoxActive = (ev: {
+    clientX: number;
+    clientY: number;
+  }): void | false => {
     if (!box.active) {
-      if (window.innerWidth * 0.1 > ev.clientX || ev.clientX > window.innerWidth * 0.9 || window.innerHeight * 0.1 > ev.clientY || ev.clientY > window.innerHeight * 0.9) {
+      if (
+        window.innerWidth * 0.1 > ev.clientX ||
+        ev.clientX > window.innerWidth * 0.9 ||
+        window.innerHeight * 0.1 > ev.clientY ||
+        ev.clientY > window.innerHeight * 0.9
+      ) {
         setBox({
           active: false,
           location: {
             x: ev.clientX,
             y: ev.clientY,
-          }
-        })
-        return false
+          },
+        });
+        return false;
       }
     }
 
-    setBox({ active: true, location: { x: ev.clientX, y: ev.clientY } })
-  }
+    setBox({ active: true, location: { x: ev.clientX, y: ev.clientY } });
+  };
 
   const makeGuess = async (char: string) => {
-    const imageSizeX = (window.innerWidth - window.innerWidth * 0.2);
-    const imageSizeY = (window.innerHeight - window.innerHeight * 0.2);
+    const imageSizeX = window.innerWidth - window.innerWidth * 0.2;
+    const imageSizeY = window.innerHeight - window.innerHeight * 0.2;
 
     console.log(imageSizeY);
 
-
     const guessPercentage = {
-      x: (box.location.x - (window.innerWidth - window.innerWidth * 0.8)) / imageSizeX,
-      y: (box.location.y - (window.innerHeight - window.innerHeight * 0.8)) / imageSizeY,
-    }
+      x:
+        (box.location.x - (window.innerWidth - window.innerWidth * 0.8)) /
+        imageSizeX,
+      y:
+        (box.location.y - (window.innerHeight - window.innerHeight * 0.8)) /
+        imageSizeY,
+    };
 
     switch (true) {
       case window.innerWidth <= 1800 && window.innerWidth > 1300:
-        localStorage.setItem("dimension", "1800")
+        localStorage.setItem("dimension", "1800");
         break;
       case window.innerWidth < 1300 && window.innerWidth > 840:
-        localStorage.setItem("dimension", "1200")
+        localStorage.setItem("dimension", "1200");
         break;
       case window.innerWidth < 840 && window.innerWidth > 600:
-        localStorage.setItem("dimension", "800")
+        localStorage.setItem("dimension", "800");
         break;
       case window.innerWidth < 600 && window.innerWidth > 400:
-        localStorage.setItem("dimension", "600")
+        localStorage.setItem("dimension", "600");
         break;
       case window.innerWidth < 400:
-        localStorage.setItem("dimension", "400")
+        localStorage.setItem("dimension", "400");
         break;
       default:
-        localStorage.setItem("dimension", "default")
+        localStorage.setItem("dimension", "default");
         break;
     }
 
     if (!localStorage.getItem("initName")) {
-      localStorage.setItem("initName", randName)
+      localStorage.setItem("initName", randName);
     }
 
-    const picture = pictures.pictures[0]
+    const picture = pictures.pictures[0];
 
     try {
-      const result = await ApiCall.makeGuess({ x: guessPercentage.x, y: guessPercentage.y, name: char, pictureId: picture.id })
+      const result = await ApiCall.makeGuess({
+        x: guessPercentage.x,
+        y: guessPercentage.y,
+        name: char,
+        pictureId: picture.id,
+      });
 
-      setWinState(result.data.hit ? true : false)
+      setWinState(result.data.hit ? true : false);
     } catch (error) {
       console.log(error);
     }
 
-    return false
-  }
+    return false;
+  };
 
   return (
-    <div className={styles.body} >
-      <div className={styles.box} style={{ position: "absolute", top: box.location.y - 50, left: box.location.x - 50, display: box.active ? "block" : "none" }}>
-        <div className='guessOptions' style={{ transform: (window.innerWidth - box.location.x > window.innerWidth * 0.5) ? "translateX(100px)" : "translateX(-100px)" }}>
-          {pictures.pictures[0].characters.map((char) =>
-            <option value={pictures.pictures[0].characters.indexOf(char)} style={{ backgroundColor: "teal" }} onClick={() => makeGuess(char.name)}>{char.name}</option>
-          )}
+    <div className={styles.body}>
+      <div
+        className={styles.box}
+        style={{
+          position: "absolute",
+          top: box.location.y - 50,
+          left: box.location.x - 50,
+          display: box.active ? "block" : "none",
+        }}
+      >
+        <div
+          className="guessOptions"
+          style={{
+            transform:
+              window.innerWidth - box.location.x > window.innerWidth * 0.5
+                ? "translateX(100px)"
+                : "translateX(-100px)",
+          }}
+        >
+          {pictures.pictures[0].characters.map((char) => (
+            <option
+              value={pictures.pictures[0].characters.indexOf(char)}
+              style={{ backgroundColor: "teal" }}
+              onClick={() => makeGuess(char.name)}
+            >
+              {char.name}
+            </option>
+          ))}
         </div>
       </div>
       <div className={styles.picContainer} onClick={toggleBoxActive}>
-        <img src="../assets/images/waldoMuseum.webp" alt="waldoMuseum"
-        />
+        <img src="../assets/images/waldoMuseum.webp" alt="waldoMuseum" />
       </div>
-      <p>
-        {`bla: ${winState}`}
-      </p>
+      <p>{`bla: ${winState}`}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
